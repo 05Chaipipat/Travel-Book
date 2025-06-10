7072285f4d9c7fcbfbbb5ac21a84d82b9956f2ba
05Chaipipat <150114910+05Chaipipat@users.noreply.github.com>
2025-06-10 15:16:52 +0700

    Add con1

--- src/utilis/constants.js
+++ src/utilis/constants.js
@@ -1,2 +1,4 @@
 
-export const BASE_URL =import.meta.env.VITE_BACKEND_URL || 'http://localhost:8000'
+// export const BASE_URL =import.meta.env.VITE_BACKEND_URL || 'http://localhost:8000'
+
+export const BASE_URL="https://travel-book-backend-ypih.onrender.com/"
